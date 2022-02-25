42de3264ed96d8bbedb9b5365e345be055f82497
김영범 <bluewings109@gmail.com>
2022-02-25 22:26:15 +0900

    날씨정보 response code 200 아닌 경우 에러처리

--- js/weather.js
+++ js/weather.js
@@ -6,17 +6,21 @@ function onFailGettingWeather(){
 function onPassGeo(response){
   const lat = response.coords.latitude;
   const lon = response.coords.longitude;
-  const API_KEY = "YOUR_API_KEYS";
+  const API_KEY = "YOUR_API_KEY";
 
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
 
   fetch(url).then(response => response.json()).then(response => {
-    const locationName = response.name;
-    const temperature = response.main.temp;
-    const weather = response.weather[0].description;
+    if(response.cod === 200){
+      const locationName = response.name;
+      const temperature = response.main.temp;
+      const weather = response.weather[0].description;
 
-    const weatherDiv = document.body.querySelector("#weather");
-    weatherDiv.innerText=`${locationName} / ${weather} /${temperature}℃`
+      const weatherDiv = document.body.querySelector("#weather");
+      weatherDiv.innerText=`${locationName} / ${weather} /${temperature}℃`
+    } else {
+      onFailGettingWeather();
+    }
   }).catch(() => onFailGettingWeather);
 
 }
